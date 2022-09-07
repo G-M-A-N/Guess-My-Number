ac817f974734ae9d35d0a719b2e9e80af1a7a3b0
G-M-A-N <gmanngesh@gmail.com>
2022-09-08 00:07:26 +0300

    click eventlistener added to 'check' button

--- starter/script.js
+++ starter/script.js
@@ -1,5 +1,5 @@
 'use strict';
-console.log(document.querySelector('.message'));
+/*console.log(document.querySelector('.message'));
 console.log(document.querySelector('.message').textContent);
 
 document.querySelector('.message').textContent = 'enter a number 🦾';
@@ -7,3 +7,8 @@ document.querySelector('.message').textContent = 'enter a number 🦾';
 document.querySelector('.number').textContent = 'G';
 
 document.querySelector('.guess').value = 23;
+*/
+
+//******queryy selector is used to select html elements based on their class names or their ID******////
+
+document.querySelector('.check').addEventListener('click');
