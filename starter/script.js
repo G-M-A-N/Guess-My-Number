9e589f4ae12f10a0c6ad1390f541005e000fe079
G-M-A-N <gmanngesh@gmail.com>
2022-09-08 04:04:25 +0300

    element number assigned to private number

--- starter/script.js
+++ starter/script.js
@@ -12,19 +12,22 @@ document.querySelector('.guess').value = 23;
 //******queryy selector is used to select html elements based on their class names or their ID******////
 
 const privateNumber = Math.trunc(Math.random() * 20 + 1);
+document.querySelector('.number').value = privateNumber;
 
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
-  console.log(typeof guess, guess);
 
   if (!guess) {
     document.querySelector('.message').textContent =
       '♨ You havent picked a number';
-  } else if (privateNumber === guess) {
+  } else if (guess === privateNumber) {
     document.querySelector('.message').textContent =
       'You Guessed The Correct Number';
-  } else if (privateNumber > guess) {
-    document.querySelector('.message').textContent = 'The Number You Picked is High ☝';
-  }else if (privateNumber < guess) {
-    document.querySelector('.message').textContent = 'The Number You Picked is Low 👇';
+  } else if (guess > privateNumber) {
+    document.querySelector('.message').textContent =
+      'The Number You Picked is High ☝';
+  } else if (guess < privateNumber) {
+    document.querySelector('.message').textContent =
+      'The Number You Picked is Low 👇';
+  }
 });
