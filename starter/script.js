b41b041722bdfa2c9d0dc0f501e11363117bc802
G-M-A-N <gmanngesh@gmail.com>
2022-09-11 03:08:30 +0300

    private number redeclared in let variable

--- starter/script.js
+++ starter/script.js
@@ -48,10 +48,10 @@ document.querySelector('.check').addEventListener('click', function () {
 
 document.querySelector('.again').addEventListener('click', function () {
   document.querySelector('body').style.backgroundColor = '#222';
-  document.querySelector('.score').textContent = 20;
+  let privateNumber = Math.trunc(Math.random() * 20 + 1);
   document.querySelector('.number').style.width = '15rem';
   document.querySelector('.message').textContent = 'Start Guessing.......';
-  document.querySelector('.highscore').textContent = 0;
+  // document.querySelector('.highscore').textContent = 0;
   document.querySelector('.guess').value = 0;
   document.querySelector('.number').textContent = '?';
-// });
+});
